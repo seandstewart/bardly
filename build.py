5597eff8110accf6cec4d180945f2d7a52c01dba
Sean Stewart <sean_stewart@me.com>
2020-08-15 16:31:04 -0400

    Optimize font sizes for mobile devices to limit line overflow.

--- build.py
+++ build.py
@@ -5,10 +5,10 @@ from mkdocs.config import load_config
 from mkdocs.commands import build
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(
         stream=sys.stdout,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=0
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=0,
     )
     build.build(load_config())
