434587e2fa39ab494d7be30098401f1237cb10a3
Sean Stewart <sean_stewart@me.com>
2021-02-25 08:30:57 -0500

    Pre-render the plays with concurrency at files collection.

--- bardly/plugin.py
+++ bardly/plugin.py
@@ -1,16 +1,34 @@
 import pathlib
+from concurrent.futures import ProcessPoolExecutor
 
 from iambic import parse, render
 from mkdocs.plugins import BasePlugin
-from mkdocs.structure.files import File
+from mkdocs.structure.files import File, Files
 from mkdocs.config import Config
 from mkdocs.structure.pages import Page
 
 
 class IambicPlugin(BasePlugin):
-    def on_page_read_source(self, page: Page, config: Config):
-        file: File = page.file
+
+    _rendered = {}
+    _pool = ProcessPoolExecutor(10)
+
+    @staticmethod
+    def _process_file(file: File):
         if file.name in {"uncut", "12-player", "12-player-alt", "11-player"}:
             text = pathlib.Path(file.abs_src_path).read_text()
             play = parse.text(text)
-            return render.markdown(play)
+            return file.abs_src_path, render.markdown(play)
+        return file.abs_src_path, None
+
+    def on_files(self, files: Files, config: Config):
+
+        self._rendered = {
+            path: md
+            for path, md in self._pool.map(self._process_file, files)
+            if md
+        }
+
+    def on_page_read_source(self, page: Page, config: Config):
+        file: File = page.file
+        return self._rendered.get(file.abs_src_path)
